edd54d13a92fdb841aca8c89145616d0e68771c0
tenudotexe <tenusal.t.headley@gmail.com>
2025-04-09 18:46:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    
+    try {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbzfRgrpt92nNW4lLiZNB2is15W5IExzkc5JSMx76xpIhQ15_VEvrkhTuj_1HrCIhq6h/exec', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        
+        if (response.ok) {
+            alert('Form submitted successfully!');
+            form.reset();
+        } else {
+            alert('Failed to submit the form.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred while submitting the form.');
+    }
+});
