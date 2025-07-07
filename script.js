2b04b99c1f471b6de6510db367b19e7b65288fb9
tenudotexe <tenusal.t.headley@gmail.com>
2025-07-07 10:14:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', async (e) => {
     const data = Object.fromEntries(formData.entries());
     
     try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbw1qLtxhlqNpKo8baGtrIm1BEp0sfi7ex9ENFU3aj3DDB1qQcybOkVpew5SSYMWaAb2/exec', {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbwOfHNGRanFvGIunh2Rmt2Crm4G_dUnukR3ViwwDY36ee7pfj_iFLuvlYPCytAglne9/exec', {
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
